d38119f173c5f44a69dbceaeff8f82ea1964e7fb
Agustina <102815348+agustinamaraz@users.noreply.github.com>
2023-06-17 13:07:25 -0300

    update usuario.route.js

--- proybackendgrupo8/routes/usuario.route.js
+++ proybackendgrupo8/routes/usuario.route.js
@@ -4,6 +4,10 @@ const router = express.Router();
 const usuarioCtrl = require('./../controllers/usuario.controller');
 // definiendo rutas
 router.post('/', usuarioCtrl.createUsuario);
+router.put('/:id', usuarioCtrl.editUsuario);
+router.delete('/:id', usuarioCtrl.deleteUsuario);
+router.get('/:id', usuarioCtrl.getUsuario);
+router.get('/', usuarioCtrl.getUsuarios);
 router.post('/login', usuarioCtrl.loginUsuario);
 //exportacion del modulo de rutas
 module.exports = router;
