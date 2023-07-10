916326ad3516933cc58690ad6b9412af97bba857
Agustina <102815348+agustinamaraz@users.noreply.github.com>
2023-07-09 22:35:58 -0300

    update my branch

--- proyfrontendgrupo8/src/app/services/login.service.ts
+++ proyfrontendgrupo8/src/app/services/login.service.ts
@@ -39,6 +39,7 @@ export class LoginService {
   public login(username: string, password: string): Observable<any> {
     const httpOption = {
       headers: new HttpHeaders({
+        //'access-control-allow-origin': 'http://localhost:4200',
         'Content-Type': 'application/json'
       })
     }
