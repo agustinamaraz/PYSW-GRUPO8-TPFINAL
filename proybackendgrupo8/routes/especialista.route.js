aaae364a6ebf294e5ee24c65aa1d9a87b513532a
Emilia <94980367+emival3@users.noreply.github.com>
2023-07-11 19:29:53 -0300

    especialista

--- proybackendgrupo8/routes/especialista.route.js
+++ proybackendgrupo8/routes/especialista.route.js
@@ -3,11 +3,12 @@ const router = express.Router();
 //defino controlador para el manejo de CRUD
 const especialistaCtrl = require('./../controllers/especialista.controller.js');
 // definiendo rutas
+router.get('/', especialistaCtrl.getEspecialistas);
 router.post('/', especialistaCtrl.createEspecialista);
-router.get('/dni', especialistaCtrl.getEspecialistaDni);
+router.get('/:dni', especialistaCtrl.getEspecialistaDni);
 router.put('/:id', especialistaCtrl.editEspecialista);
 router.delete('/:id', especialistaCtrl.deleteEspecialista);
 router.get('/:id', especialistaCtrl.getEspecialista);
-router.get('/', especialistaCtrl.getEspecialistas);
+
 //exportacion del modulo de rutas
 module.exports = router;
